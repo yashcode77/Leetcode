09c6ade0049c6385eb424d1239dfaa0784a1b6f4
Yash Chhaproo <96254415+yashcode77@users.noreply.github.com>
2022-12-19 17:25:11 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (81.24%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        while(num>0)
+        {
+            while(num>0)
+            {
+                sum += (num%10); num/=10;
+            }
+            if(sum>9) 
+            {
+                num=sum; sum=0; 
+            } 
+        }
+
+        return sum;
+    }
+}
