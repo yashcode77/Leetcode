c8e46e3191703fbc85f6bfb76088af71f96e01f7
Yash Chhaproo <yashchhaproo@gmail.com>
2023-08-19 21:31:23 +0530

    Time: 20 ms (72.27%), Space: 19.2 MB (72.69%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=1;
+        while(j<nums.size()){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }else if(nums[i]!=0){
+                i++;
+            }
+            j++;
+        }
+    }
+};
